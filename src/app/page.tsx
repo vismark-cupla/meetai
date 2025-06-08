28f9332ae89b074a7435e4ce05a4246e6d26beaf
Vismark Cupla <vismark.cupla@yahoo.com>
2025-06-09 03:13:30 +0800

    main
    * test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Button variant="destructive">Click Me</Button>;
+  return <Button variant="destructive">Test</Button>;
 }
